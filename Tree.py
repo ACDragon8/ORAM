550c6216c2f03cd7a5544929c1dff61e754b291e
Hao Deng <haodeng8@gmail.com>
2025-02-10 17:36:14 -0800

    initial

---
+++ Tree.py
@@ -0,0 +1,19 @@
+class Node:
+    def __init__(self, item):
+        self.item = item
+        self.left = None
+        self.right = None
+
+    def add(self,item):
+        
+
+class Tree:
+    def __init__(self):
+        self.head = None
+
+    def add(item):
+        if(self.head == None):
+            self.head = Node(item)
+            return True
+        elif()
+    
